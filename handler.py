70b4863d351650386ab669fbcee6afd415c9b5be
Cag <30646014+caggle@users.noreply.github.com>
2018-10-15 16:27:32 -0700

    Update handler.py to trigger error

--- handler.py
+++ handler.py
@@ -9,3 +9,6 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     logger.info("Your cron function " + name + " ran at " + str(current_time))
+    
+    # trigger error here
+    i = 7 / 0
