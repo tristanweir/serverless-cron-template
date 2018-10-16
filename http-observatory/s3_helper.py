5e11c75f4235a778fc59c7b328950f94e686dbd7
Tristan Weir <tweir@mozilla.com>
2018-10-16 16:59:28 -0700

    Cast the JSON results as a string

--- http-observatory/s3_helper.py
+++ http-observatory/s3_helper.py
@@ -8,7 +8,8 @@ client = boto3.client('s3')
 def send_to_s3(hostname, scan_json):
     key = "{}.{}".format(hostname, "json")
     bucket = 'observatoryresults'
-    client.put_object(Body=scan_json, Bucket=bucket,
+    binary_scan_json = bytearray(scan_json.encode('utf-8'))
+    client.put_object(Body=binary_scan_json, Bucket=bucket,
                       Key=key, ACL='public-read')
     url = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
     logging.info("Uploaded result file to URL: {}".format(url))
