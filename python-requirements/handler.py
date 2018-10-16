4898621d3d5d87c5a9d0c3785205d57f05a5e31d
jdow <jdow@mozilla.com>
2018-10-16 12:57:29 -0700

    Example adding a requirement that is not pure python. (#13)
    * adding non-pure-python requirement that requires extra packages to be installed
    
    * fixing order of imports

--- python-requirements/handler.py
+++ python-requirements/handler.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+import ldap
 import requests  # this is an example of an import
 
 logger = logging.getLogger(__name__)
@@ -10,6 +11,8 @@ def run(event, context):
     current_time = datetime.datetime.now().time()
     name = context.function_name
     status_code = requests.get('https://mozilla.org/').status_code
+    ldap_conn = ldap.initialize('ldaps://ldap.mozilla.org:636')
 
+    logger.info("LDAP connection: " + str(ldap_conn))
     logger.info("Your cron function " + name + " ran at " + str(current_time))
     logger.info("Status code returned by mozilla.org: " + str(status_code))
