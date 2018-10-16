d3aa437d79638760f72a4e684ff1fe5db0b5c3ae
Justin Dow <jdow@mozilla.com>
2018-10-16 16:08:56 -0700

    adding logic to handler

--- http-observatory/handler.py
+++ http-observatory/handler.py
@@ -1,6 +1,9 @@
 import datetime
 import logging
 
+from observatory_scanner import ObservatoryScanner
+from randomizer import Randomizer
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -9,7 +12,11 @@ import s3_helper
 
 def run(event, context):
     # Test out S3 upload capability
-    scan_result = "test"
-    hostname = "www.mozilla.org"
+    url = 'https://raw.githubusercontent.com/mozilla/http-observatory-dashboard/master/httpobsdashboard/conf/sites.json'
+    random_site = Randomizer(url)
+    scanner = ObservatoryScanner()
+    scan_result = scanner.scan(random_site.next())
+    logger.info(scan_result)
 
-    send_to_s3(hostname, scan_result)
+
+#    send_to_s3(hostname, scan_result)
