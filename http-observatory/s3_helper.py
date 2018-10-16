9feb4e21cdda3877f879ce098025a7b68996fcdb
Tristan Weir <tweir@mozilla.com>
2018-10-16 16:20:26 -0700

    Make S3 Permissions work It also won't error if the S3 bucket already exists

--- http-observatory/s3_helper.py
+++ http-observatory/s3_helper.py
@@ -6,7 +6,7 @@ client = boto3.client('s3')
 
 def send_to_s3(hostname, scan_json):
     key = "{}.{}".format(hostname, "json")
-    bucket = 'observatory-results-2'
+    bucket = 'observatoryresults'
     client.put_object(Body=scan_json, Bucket=bucket,
                       Key=key, ACL='public-read')
     url = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
