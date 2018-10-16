3b20daa17ff6f911341ba0fc9485f8094250d1c7
Cag <culucenk@mozilla.com>
2018-10-16 09:06:24 -0700

    Error notification via CloudWatch and SNS

---
+++ error-notification/handler.py
@@ -0,0 +1,14 @@
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def run(event, context):
+    current_time = datetime.datetime.now().time()
+    name = context.function_name
+    logger.info("Your cron function " + name + " ran at " + str(current_time))
+    
+    # trigger error here
+    i = 7 / 0
