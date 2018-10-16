dda614a49024b423ab59e9c3541892245c06defb
Tristan Weir <tweir@mozilla.com>
2018-10-16 15:51:27 -0700

    Trying to make S3 permissions work

--- http-observatory/handler.py
+++ http-observatory/handler.py
@@ -1,10 +1,12 @@
 import datetime
 import logging
+import s3_helper
+
+from s3_helper import send_to_s3
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-import s3_helper
 
 
 def run(event, context):
--- http-observatory/s3_helper.py
+++ http-observatory/s3_helper.py
@@ -4,10 +4,9 @@ import os
 
 client = boto3.client('s3')
 
-
 def send_to_s3(hostname, scan_json):
     key = "{}.{}".format(hostname, "json")
-    bucket = 'observatory-results-1'
+    bucket = 'observatory-results-2'
     client.put_object(Body=scan_json, Bucket=bucket,
                       Key=key, ACL='public-read')
     url = "https://s3.amazonaws.com/{}/{}".format(bucket, key)
